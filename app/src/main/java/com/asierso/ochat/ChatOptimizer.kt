6dcbe14205257ec77fb0eea21dafe418c041d572
Asierso <33327643+Asierso@users.noreply.github.com>
2024-10-07 09:41:06 +0200

    Doc: Optimizer javadoc

--- app/src/main/java/com/asierso/ochat/ChatOptimizer.kt
+++ app/src/main/java/com/asierso/ochat/ChatOptimizer.kt
@@ -5,30 +5,38 @@ import com.asierso.ochat.api.models.LlamaMessage
 import com.asierso.ochat.models.Conversation
 
 class ChatOptimizer (var conversation: Conversation) {
-    //Constants for chat optimization
+    //Constants for chat optimization (min chats and max characters threshold)
     private val startOptimizerChat = 3
     private val maxChars = 1000
+
+    //Final optimized chat
     private var optimizedchat = arrayListOf<LlamaMessage>()
+
+
     init{
+        //No optimize chat if less big than optimizer start threshold
         if(conversation.chat.size <= startOptimizerChat)
             optimizedchat = conversation.chat
         else {
-            optimizedchat = optimizeChat(startOptimizerChat)
+            optimizedchat = optimizeChat(startOptimizerChat) //Start optimize chat of base threshold
         }
     }
 
     private fun optimizeChat(maxChat: Int) : ArrayList<LlamaMessage> {
         var localoptimizedchat = arrayListOf<LlamaMessage>()
         var charCount = 0
+
+        //Add message from threshold (maxChat) to end
         for (i in conversation.chat.size - maxChat until conversation.chat.size) {
             charCount+=conversation.chat[i].content.length
             localoptimizedchat.add(conversation.chat[i])
         }
 
+        //Detect if context might be more (chats with small char count)
         if(charCount <= maxChars * .5 && conversation.chat.size > maxChat + 1)
             localoptimizedchat = optimizeChat(maxChat+1)
 
-        //Detects if optimization is needed and optimize chat
+        //Detects if more optimization is needed and reduce chat
         while(charCount > maxChars && localoptimizedchat.size >= 3)
             charCount-=localoptimizedchat.removeAt(0).content.length
 
